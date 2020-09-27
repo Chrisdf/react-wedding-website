eae4a354c675befb325d264c7883bda5ab49b03c
Chris Frantz <chrisfrantz96@gmail.com>
2020-09-27 00:18:30 -0500

    Updates

---
+++ precache-manifest.160cef44edbb79f1ab31d3242b260870.js
@@ -0,0 +1,114 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c989c10d2f8fe03514c981123538a2fa",
+    "url": "/react-wedding-website/index.html"
+  },
+  {
+    "revision": "3884144750962fd5e533",
+    "url": "/react-wedding-website/static/css/2.3d7b2185.chunk.css"
+  },
+  {
+    "revision": "9dfbcf9fd0f6ccbbb1e5",
+    "url": "/react-wedding-website/static/css/main.12f4a39d.chunk.css"
+  },
+  {
+    "revision": "3884144750962fd5e533",
+    "url": "/react-wedding-website/static/js/2.0e2a1130.chunk.js"
+  },
+  {
+    "revision": "9dfbcf9fd0f6ccbbb1e5",
+    "url": "/react-wedding-website/static/js/main.a10b413e.chunk.js"
+  },
+  {
+    "revision": "c1fb09db1df2d823b550",
+    "url": "/react-wedding-website/static/js/runtime~main.f488c6ef.js"
+  },
+  {
+    "revision": "542ae65c6ffd3ba9af83120070bf569a",
+    "url": "/react-wedding-website/static/media/balmorhea.542ae65c.mp4"
+  },
+  {
+    "revision": "d495ab751682cdc2c92482f07c9f0ebd",
+    "url": "/react-wedding-website/static/media/balmorhea.d495ab75.jpg"
+  },
+  {
+    "revision": "a1edd550c6515f98080e214536175b91",
+    "url": "/react-wedding-website/static/media/banner.a1edd550.jpg"
+  },
+  {
+    "revision": "9f7345a985e4136b6143d4bb965934e3",
+    "url": "/react-wedding-website/static/media/chapel.9f7345a9.jpg"
+  },
+  {
+    "revision": "bb320344e73f8ced3b2e3bb4cbd62afc",
+    "url": "/react-wedding-website/static/media/chris-cropped.bb320344.jpg"
+  },
+  {
+    "revision": "123b7d37cd8d85017b593ad45c328a13",
+    "url": "/react-wedding-website/static/media/close.123b7d37.svg"
+  },
+  {
+    "revision": "496541afab21ee75f8b393090a4ddc3c",
+    "url": "/react-wedding-website/static/media/cocktail.496541af.png"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/react-wedding-website/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/react-wedding-website/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/react-wedding-website/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/react-wedding-website/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/react-wedding-website/static/media/fontawesome-webfont.fee66e71.woff"
+  },
+  {
+    "revision": "5322b1e1b9cda3b747e343561eedb918",
+    "url": "/react-wedding-website/static/media/img-1.5322b1e1.jpg"
+  },
+  {
+    "revision": "38cf848f6a1362bc2a568712cf9319d1",
+    "url": "/react-wedding-website/static/media/img-2.38cf848f.jpg"
+  },
+  {
+    "revision": "1fb8c67fcc1bce661d46e326ec4397ee",
+    "url": "/react-wedding-website/static/media/img-3.1fb8c67f.jpg"
+  },
+  {
+    "revision": "ea9d376831f4f827135326c11d4e7031",
+    "url": "/react-wedding-website/static/media/img-4.ea9d3768.jpg"
+  },
+  {
+    "revision": "fe7ac27a737abc1a896931ff78f0ef43",
+    "url": "/react-wedding-website/static/media/malendy-cropped.fe7ac27a.jpg"
+  },
+  {
+    "revision": "95d1d9f73d3bce3364cd355bfc077829",
+    "url": "/react-wedding-website/static/media/reception.95d1d9f7.png"
+  },
+  {
+    "revision": "b7c9e1e479de3b53f1e4e30ebac2403a",
+    "url": "/react-wedding-website/static/media/slick.b7c9e1e4.woff"
+  },
+  {
+    "revision": "ced611daf7709cc778da928fec876475",
+    "url": "/react-wedding-website/static/media/slick.ced611da.eot"
+  },
+  {
+    "revision": "d41f55a78e6f49a5512878df1737e58a",
+    "url": "/react-wedding-website/static/media/slick.d41f55a7.ttf"
+  },
+  {
+    "revision": "f97e3bbf73254b0112091d0192f17aec",
+    "url": "/react-wedding-website/static/media/slick.f97e3bbf.svg"
+  }
+]);
