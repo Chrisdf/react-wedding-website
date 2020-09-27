ed08dc3a746e21a09b19fc388b0bc2c930d9494b
Chris Frantz <chrisfrantz96@gmail.com>
2020-09-27 13:38:02 -0500

    Updates

---
+++ precache-manifest.552d8ce478e7fa01e2e95650508993db.js
@@ -0,0 +1,154 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0dd964ed207befbec2dd04398c64fd09",
+    "url": "thefrantzwedding.com/index.html"
+  },
+  {
+    "revision": "ae5665870319df48f0d6",
+    "url": "thefrantzwedding.com/static/css/2.72ac0b79.chunk.css"
+  },
+  {
+    "revision": "c255772f9da6106ea2de",
+    "url": "thefrantzwedding.com/static/css/main.cca7ab5f.chunk.css"
+  },
+  {
+    "revision": "ae5665870319df48f0d6",
+    "url": "thefrantzwedding.com/static/js/2.0bc5155e.chunk.js"
+  },
+  {
+    "revision": "c255772f9da6106ea2de",
+    "url": "thefrantzwedding.com/static/js/main.894c2327.chunk.js"
+  },
+  {
+    "revision": "08af124c93818914fd41",
+    "url": "thefrantzwedding.com/static/js/runtime~main.cfc952d0.js"
+  },
+  {
+    "revision": "b0130703d993fa507dac0270130c5a2a",
+    "url": "thefrantzwedding.com/static/media/IMG_2356.b0130703.jpg"
+  },
+  {
+    "revision": "a9ae1deafb11c633b608d47667ab6fcd",
+    "url": "thefrantzwedding.com/static/media/IMG_2365.a9ae1dea.jpg"
+  },
+  {
+    "revision": "1e2f6852692bb68c81e918777365e1ca",
+    "url": "thefrantzwedding.com/static/media/IMG_6529.1e2f6852.jpg"
+  },
+  {
+    "revision": "37d3ccaabb97fc62819154b49972b67f",
+    "url": "thefrantzwedding.com/static/media/IMG_6601.37d3ccaa.jpg"
+  },
+  {
+    "revision": "82e43fc1ac9fcf2575536a820bc9367b",
+    "url": "thefrantzwedding.com/static/media/IMG_6659.82e43fc1.jpg"
+  },
+  {
+    "revision": "268bb20afd2794c7ec71d72f447adf64",
+    "url": "thefrantzwedding.com/static/media/IMG_7612.268bb20a.jpg"
+  },
+  {
+    "revision": "543282963236ec578687a83487d3b302",
+    "url": "thefrantzwedding.com/static/media/IMG_7636.54328296.jpg"
+  },
+  {
+    "revision": "3d9908b0c000d61852b77254f7b7af8d",
+    "url": "thefrantzwedding.com/static/media/IMG_7640.3d9908b0.jpg"
+  },
+  {
+    "revision": "edc334cf2f72f877cb8a3b7baaa64180",
+    "url": "thefrantzwedding.com/static/media/atire.edc334cf.png"
+  },
+  {
+    "revision": "542ae65c6ffd3ba9af83120070bf569a",
+    "url": "thefrantzwedding.com/static/media/balmorhea.542ae65c.mp4"
+  },
+  {
+    "revision": "d495ab751682cdc2c92482f07c9f0ebd",
+    "url": "thefrantzwedding.com/static/media/balmorhea.d495ab75.jpg"
+  },
+  {
+    "revision": "a1edd550c6515f98080e214536175b91",
+    "url": "thefrantzwedding.com/static/media/banner.a1edd550.jpg"
+  },
+  {
+    "revision": "9f7345a985e4136b6143d4bb965934e3",
+    "url": "thefrantzwedding.com/static/media/chapel.9f7345a9.jpg"
+  },
+  {
+    "revision": "bb320344e73f8ced3b2e3bb4cbd62afc",
+    "url": "thefrantzwedding.com/static/media/chris-cropped.bb320344.jpg"
+  },
+  {
+    "revision": "123b7d37cd8d85017b593ad45c328a13",
+    "url": "thefrantzwedding.com/static/media/close.123b7d37.svg"
+  },
+  {
+    "revision": "496541afab21ee75f8b393090a4ddc3c",
+    "url": "thefrantzwedding.com/static/media/cocktail.496541af.png"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "thefrantzwedding.com/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "acf3dcb7ff752b5296ca23ba2c7c2606",
+    "url": "thefrantzwedding.com/static/media/fontawesome-webfont.acf3dcb7.svg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "thefrantzwedding.com/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "thefrantzwedding.com/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "thefrantzwedding.com/static/media/fontawesome-webfont.fee66e71.woff"
+  },
+  {
+    "revision": "5322b1e1b9cda3b747e343561eedb918",
+    "url": "thefrantzwedding.com/static/media/img-1.5322b1e1.jpg"
+  },
+  {
+    "revision": "38cf848f6a1362bc2a568712cf9319d1",
+    "url": "thefrantzwedding.com/static/media/img-2.38cf848f.jpg"
+  },
+  {
+    "revision": "1fb8c67fcc1bce661d46e326ec4397ee",
+    "url": "thefrantzwedding.com/static/media/img-3.1fb8c67f.jpg"
+  },
+  {
+    "revision": "ea9d376831f4f827135326c11d4e7031",
+    "url": "thefrantzwedding.com/static/media/img-4.ea9d3768.jpg"
+  },
+  {
+    "revision": "fe7ac27a737abc1a896931ff78f0ef43",
+    "url": "thefrantzwedding.com/static/media/malendy-cropped.fe7ac27a.jpg"
+  },
+  {
+    "revision": "95d1d9f73d3bce3364cd355bfc077829",
+    "url": "thefrantzwedding.com/static/media/reception.95d1d9f7.png"
+  },
+  {
+    "revision": "b7c9e1e479de3b53f1e4e30ebac2403a",
+    "url": "thefrantzwedding.com/static/media/slick.b7c9e1e4.woff"
+  },
+  {
+    "revision": "ced611daf7709cc778da928fec876475",
+    "url": "thefrantzwedding.com/static/media/slick.ced611da.eot"
+  },
+  {
+    "revision": "d41f55a78e6f49a5512878df1737e58a",
+    "url": "thefrantzwedding.com/static/media/slick.d41f55a7.ttf"
+  },
+  {
+    "revision": "f97e3bbf73254b0112091d0192f17aec",
+    "url": "thefrantzwedding.com/static/media/slick.f97e3bbf.svg"
+  },
+  {
+    "revision": "91272941a9fe0e4b632ad8d1d7ba7d8f",
+    "url": "thefrantzwedding.com/static/media/uber-lyft.91272941.jpg"
+  }
+]);
